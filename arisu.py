a1e9293455ebf9f78361e5e07a0c2400326eb68e
Alan Miles <thesorton@gmail.com>
2019-10-16 18:50:24 -0700

    kick and ban

--- arisu.py
+++ arisu.py
@@ -5,18 +5,21 @@ from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv()
 
-# Import bot plugins.
+# Import bot "plugins".
 from plugins import (color, help, img, log, mod, util, welcome)
 
+# Vars
 token = os.environ.get("TOKEN")
 prefix = os.environ.get("PREFIX")
-
 client = discord.Client()
 
+# Startup
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
+
+# a!hello
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -26,18 +29,22 @@ async def on_message(message):
         await message.channel.send('Hello! The prefix is ' + prefix)
 
 # Util
-
 ## Code
 @client.event
 async def on_message(message):
     if message.content.startswith(prefix + 'code'):
         await util.code_command(message)
 
+    elif message.content.startswith(prefix + 'kick'):
+        await mod.kick(message)
+
+    elif message.content.startswith(prefix + 'ban'):
+        await mod.ban(message)
 ## Avatar
-    if message.content.startswith(prefix + 'avatar'):
+    elif message.content.startswith(prefix + 'avatar'):
         await img.avatar_command(message)
 
-# Welome events
+# Welcome events
 @client.event
 async def on_member_join(member):
     await welcome.mem_join(member)
@@ -46,4 +53,5 @@ async def on_member_join(member):
 async def on_member_remove(member):
     await welcome.mem_leave(member)
 
+# Run it 
 client.run(token)
---
+++ plugins/mod.py
@@ -0,0 +1,22 @@
+import discord 
+client = discord.Client()
+server = discord.Guild
+
+# Kick
+async def kick(message):
+    if (message.mentions.__len__()>0):
+        for user in message.mentions:
+            if message.author.guild_permissions.administrator:
+                server = user.guild
+                target = server.get_member(user.id)
+                await server.kick(target)
+                await message.channel.send('**{}** has been kicked'.format(target))
+# Ban
+async def ban(message):
+    if (message.mentions.__len__()>0):
+        for user in message.mentions:
+            if message.author.guild_permissions.administrator:
+                server = user.guild
+                target = server.get_member(user.id)
+                await server.ban(target)
+                await message.channel.send('**{}** has been banned'.format(target))
