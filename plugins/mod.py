f029be20e59c9c8ec87717d5fc0eea8ef80d55e5
h4pZ <h4pz@protonmail.com>
2019-10-17 21:52:26 +0200

    Cleaning mod.py

--- plugins/mod.py
+++ plugins/mod.py
@@ -1,11 +1,11 @@
-import discord 
+import discord
 client = discord.Client()
 server = discord.Guild
 member = discord.Member
 
 # Kick
 async def kick(message):
-    if (message.mentions.__len__()>0):
+    if (len(message.mentions) > 0):
         for user in message.mentions:
             if message.author.guild_permissions.administrator:
                 server = user.guild
@@ -14,7 +14,7 @@ async def kick(message):
                 await message.channel.send('**{}** has been kicked'.format(target))
 # Ban
 async def ban(message):
-    if (message.mentions.__len__()>0):
+    if (len(message.mentions) > 0):
         for user in message.mentions:
             if message.author.guild_permissions.administrator:
                 server = user.guild
@@ -24,7 +24,7 @@ async def ban(message):
 
 # Mute
 async def mute(message):
-    if (message.mentions.__len__()>0):
+    if (len(message.mentions) > 0):
         for user in message.mentions:
             if message.author.guild_permissions.administrator:
                 server = user.guild
@@ -33,11 +33,11 @@ async def mute(message):
                 await member.add_roles(target, role)
 
 async def unmute(message):
-     if (message.mentions.__len__()>0):
+    if (len(message.mentions) > 0):
         for user in message.mentions:
             if message.author.guild_permissions.administrator:
                 server = user.guild
                 target = server.get_member(user.id)
                 role = discord.utils.get(server.roles, name="muted")
                 await member.remove_roles(target, role)
-   
+
--- plugins/welcome.py
+++ plugins/welcome.py
@@ -4,13 +4,13 @@ client = discord.Client()
 
 # Welcome 
 async def mem_join(member):
-     guild = member.guild 
+     guild = member.guild
      if guild.system_channel is not None:
-        msg = 'Welcome {0.mention}!'.format(member) 
+        msg = 'Welcome {0.mention}!'.format(member)
         await guild.system_channel.send(msg)
 
 async def mem_leave(member):
-     guild = member.guild 
+     guild = member.guild
      if guild.system_channel is not None:
-        msg = 'Bye **{0.name}**!'.format(member) 
+        msg = 'Bye **{0.name}**!'.format(member)
         await guild.system_channel.send(msg)
