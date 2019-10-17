c069637f24bafe5e5a8a6525ad3c28d273b036a5
Alan Miles <thesorton@gmail.com>
2019-10-16 21:26:30 -0700

    zippers not included

--- plugins/mod.py
+++ plugins/mod.py
@@ -1,7 +1,6 @@
 import discord 
 client = discord.Client()
 server = discord.Guild
-member = discord.Member
 
 # Kick
 async def kick(message):
@@ -21,23 +20,3 @@ async def ban(message):
                 target = server.get_member(user.id)
                 await server.ban(target)
                 await message.channel.send('**{}** has been banned'.format(target))
-
-# Mute
-async def mute(message):
-    if (message.mentions.__len__()>0):
-        for user in message.mentions:
-            if message.author.guild_permissions.administrator:
-                server = user.guild
-                target = server.get_member(user.id)
-                role = discord.utils.get(server.roles, name="muted")
-                await member.add_roles(target, role)
-
-async def unmute(message):
-     if (message.mentions.__len__()>0):
-        for user in message.mentions:
-            if message.author.guild_permissions.administrator:
-                server = user.guild
-                target = server.get_member(user.id)
-                role = discord.utils.get(server.roles, name="muted")
-                await member.remove_roles(target, role)
-   
