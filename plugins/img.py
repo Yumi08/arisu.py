bfa199c21c0a6ff7d046299a4625acaefc9511ff
h4pZ <h4pz@protonmail.com>
2019-10-18 09:43:03 +0200

    Moved avatar embed to utils and refactored img.py

--- plugins/img.py
+++ plugins/img.py
@@ -1,39 +1,27 @@
  # In dire need of optimization
-import discord
-import os
-import json
-from colorthief import ColorThief
-from discord import Webhook, AsyncWebhookAdapter
+import util
 
 # Avatar command
 async def avatar_command(message):
-        if (len(message.mentions) > 0):
+        if len(message.mentions) > 0:
             for user in message.mentions:
                 print(user.avatar_url)
-                requestor = message.author
-                name = user.name
-                avatarImage = user.avatar_url
-                os.system('curl -o .img.png {}'.format(avatarImage))
-                color_thief = ColorThief('.img.png')
-                dominant_color = color_thief.get_color(quality=1)
-                os.system('rm .img.png')
-                clr = '0x' + '%02X%02X%02X' % dominant_color
-                clr = int(clr, base=16)
-                embed = discord.Embed(title="Avatar of {}".format(name), value=requestor, color=clr)
-                embed.set_image(url=avatarImage)
-                await message.channel.send(embed=embed)
-        else:
-                requestor = message.author
-                name = message.author.name
-                avatarImage = Webhook.avatar_url_as(message.author, format=None, size=1024)
-                os.system('curl -o .img.png {}'.format(avatarImage))
-                color_thief = ColorThief('.img.png')
-                dominant_color = color_thief.get_color(quality=1)
-                os.system('rm .img.png')
-                clr = '0x' + '%02X%02X%02X' % dominant_color
-                clr = int(clr, base=16)
-                embed = discord.Embed(title="Avatar of {}".format(name), value=requestor, color=clr)
-                embed.set_image(url=avatarImage)
+                embed = utils.create_avatar_embed(message, user)
                 await message.channel.send(embed=embed)
 
+        # If the message contains users but doesn't metion them.
+        elif len(message.content.split()) > 2:
+            # Getting the users and removing nicknames that didn't match (None).
+            nicknames = message.content.split()[1:]
+            users = [util.find_member(message, nickname) for nickname in nicknames]
+            users = [user for user in users if user is not None]
+
+            for user in users:
+                print(user.avatar_url)
+                embed = utils.create_avatar_embed(message, user)
+                await message.channel.send(embed=embed)
+
+        else:
+                embed = utils.create_avatar_embed(message, message.author)
+                await message.channel.send(embed=embed)
 
--- plugins/util.py
+++ plugins/util.py
@@ -1,4 +1,6 @@
 import os
+import discord
+from colorthief import ColorThief
 
 
 async def code_command(message):
@@ -12,3 +14,18 @@ async def find_member(message, nickname):
             await member
 
     await None
+
+async def create_avatar_embed(message, user):
+    requestor = message.author
+    name = user.name
+    avatarImage = user.avatar_url
+    os.system('curl -o .img.png {}'.format(avatarImage))
+    color_thief = ColorThief('.img.png')
+    dominant_color = color_thief.get_color(quality=1)
+    os.system('rm .img.png')
+    clr = '0x' + '%02X%02X%02X' % dominant_color
+    clr = int(clr, base=16)
+    embed = discord.Embed(title="Avatar of {}".format(name), value=requestor, color=clr)
+    embed.set_image(url=avatarImage)
+
+    await embed
