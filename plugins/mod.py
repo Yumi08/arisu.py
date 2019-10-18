85575f7840c95bd8f4d7902b53ab1b1d10cb798a
h4pZ <h4pz@protonmail.com>
2019-10-18 10:54:53 +0200

    Cleaned mod.py

--- plugins/mod.py
+++ plugins/mod.py
@@ -1,43 +1,38 @@
 import discord
-client = discord.Client()
-server = discord.Guild
-member = discord.Member
 
-# Kick
+
 async def kick(message):
-    if (len(message.mentions) > 0):
+    if len(message.mentions) > 0:
         for user in message.mentions:
             if message.author.guild_permissions.administrator:
                 server = user.guild
-                target = server.get_member(user.id)
-                await server.kick(target)
-                await message.channel.send('**{}** has been kicked'.format(target))
-# Ban
-async def ban(message):
-    if (len(message.mentions) > 0):
-        for user in message.mentions:
-            if message.author.guild_permissions.administrator:
-                server = user.guild
-                target = server.get_member(user.id)
-                await server.ban(target)
-                await message.channel.send('**{}** has been banned'.format(target))
+                await server.kick(user)
+                await message.channel.send('**{}** has been kicked'.format(user))
 
-# Mute
-async def mute(message):
-    if (len(message.mentions) > 0):
+
+async def ban(message):
+    if len(message.mentions) > 0:
         for user in message.mentions:
             if message.author.guild_permissions.administrator:
                 server = user.guild
-                target = server.get_member(user.id)
+                await server.ban(user)
+                await message.channel.send('**{}** has been banned'.format(user))
+
+
+async def mute(message):
+    if len(message.mentions) > 0:
+        for user in message.mentions:
+            if message.author.guild_permissions.administrator:
+                server = user.guild
                 role = discord.utils.get(server.roles, name="silenced")
-                await member.add_roles(target, role)
+                await user.add_roles(role)
+
 
 async def unmute(message):
-    if (len(message.mentions) > 0):
+    if len(message.mentions) > 0:
         for user in message.mentions:
             if message.author.guild_permissions.administrator:
                 server = user.guild
-                target = server.get_member(user.id)
                 role = discord.utils.get(server.roles, name="silenced")
-                await member.remove_roles(target, role)
+                await user.remove_roles(role)
 
