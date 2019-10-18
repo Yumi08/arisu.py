832a619800481f33dd4ce2f005964e6379401bcd
h4pZ <h4pz@protonmail.com>
2019-10-18 07:51:54 +0200

    Remove unnecesary imports from util.py and set find_member function

--- plugins/util.py
+++ plugins/util.py
@@ -1,6 +1,14 @@
-import discord
 import os
+
 
 async def code_command(message):
     url = 'https://github.com/TheSorton/arisu.py'
     await message.channel.send(url)
+
+
+async def find_member(message, nickname):
+    for member in message.guild.members:
+        if nickname in member.nick:
+            await member
+
+    await None
