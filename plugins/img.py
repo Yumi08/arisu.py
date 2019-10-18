b9439fafa176b892ca960cd86a95419d85ffca56
h4pZ <h4pz@protonmail.com>
2019-10-18 22:52:52 +0200

    Add colour to the embed of the pape command

--- plugins/img.py
+++ plugins/img.py
@@ -43,7 +43,8 @@ async def pape(message):
     if len(response.json()['data']) > 0:
         wallpaper_url = response.json()['data'][0]['url']
         embed = discord.Embed(title='Wallpaper',
-                              description='[Source] ' + wallpaper_url)
+                              description='[Source] ' + wallpaper_url,
+                              colour='0xbe132d')
         embed = (embed.set_footer(text='This command is powered by wallhaven.cc',
                                   icon_url=footer_url)
                       .set_image(url=wallpaper_url))
